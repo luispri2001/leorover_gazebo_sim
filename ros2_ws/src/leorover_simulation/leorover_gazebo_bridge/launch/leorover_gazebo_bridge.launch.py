eb7002e5f8e83d0c2d67a4a6b036d24ad741a26d
luispri2001 <lpriel00@estudiantes.unileon.es>
2025-03-10 14:06:56 +0100

    changes to add compatibility with gps navigation in gazebo ignition

--- ros2_ws/src/leorover_simulation/leorover_gazebo_bridge/launch/leorover_gazebo_bridge.launch.py
+++ ros2_ws/src/leorover_simulation/leorover_gazebo_bridge/launch/leorover_gazebo_bridge.launch.py
@@ -7,7 +7,7 @@ def generate_launch_description():
     parameters=[]
     arguments=[
         '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
-        '/model/leorover/odometry@nav_msgs/msg/Odometry]gz.msgs.Odometry',
+        '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
         '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
         '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
         '/imu@sensor_msgs/msg/Imu[gz.msgs.IMU',
@@ -16,23 +16,27 @@ def generate_launch_description():
         '/leo/realsense_d455/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
         '/leo/realsense_d455/image@sensor_msgs/msg/Image[gz.msgs.Image',
         '/leo/realsense_d455/depth_image@sensor_msgs/msg/Image[gz.msgs.Image',
-        '/model/leorover/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
-        '/world/lunar_landscape/model/leorover/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model',
+    #    '/model/leorover/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
+        '/gps/fix@sensor_msgs/msg/NavSatFix[ignition.msgs.NavSat',
+        '/world/default/model/leorover/joint_state@sensor_msgs/msg/JointState[gz.msgs.Model',
+        '/odom/tf@tf2_msgs/msg/TFMessage[ignition.msgs.Pose_V',
+
     ]
 
     remappings=[
-        ('/clock', '/leo/clock'),
-        ('/model/leorover/odometry', '/leo/odometry'),
+        ("/odom/tf", "tf"),
+        # ('/clock', '/leo/clock'),
+        # ('/model/leorover/odometry', '/odom'),
         ('/cmd_vel', '/cmd_vel'),
         ('/scan', '/leo/scan'),
-        ('/imu', '/leo/imu'),
+        ('/imu', '/imu'),
         ('/body_camera/camera_info', '/leo/body_camera/camera_info'),
         ('/body_camera/image', '/leo/body_camera/image'),
         ('/leo/realsense_d455/camera_info', '/leo/realsense_d455/camera_info'),
         ('/leo/realsense_d455/image', '/leo/realsense_d455/image'),
         ('/leo/realsense_d455/depth_image', '/leo/realsense_d455/depth_image'),
-        ('/model/leorover/tf','/leo/tf'), # Renamed as otherwise, the leorover/odom frame will interfere with SLAM
-        ('/world/lunar_landscape/model/leorover/joint_state','/joint_states')
+    #    ('/model/leorover/tf','/leo/tf'), # Renamed as otherwise, the leorover/odom frame will interfere with SLAM
+        ('/world/default/model/leorover/joint_state','/joint_states')
     ]
 
     return LaunchDescription([
